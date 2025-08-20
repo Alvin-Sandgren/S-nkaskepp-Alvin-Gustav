e6dc810878c9b37c4b92cc18a48701f9704115cf
Erik Gabrielsson <erikgabrielsson22@gmail.com>
2025-08-20 14:45:21 +0200

    meow

--- knapp.js
+++ knapp.js
@@ -1,8 +1,8 @@
 function toggleMode(button) {
     const modeDisplay = document.getElementById("modeDisplay");
-    if (button.textContent === "Klassisk Läge") {
+    if (button.textContent === "Klassiskt Läge") {
         button.textContent = "Ryskt Läge";
-        modeDisplay.textContent = "Ryskt läge";
+        modeDisplay.textContent = "Ryskt Läge";
         console.log("Bytte till Ryskt Läge");
     } else {
         button.textContent = "Klassiskt Läge";
