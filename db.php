40f273fdd7257fc3d56e9bf59f1bd393052d95dc
Kungenskneckt <gustav.jakobsson@elev.ga.ntig.se>
2025-08-28 14:41:55 +0200

    db con

--- db.php
+++ db.php
@@ -1,6 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost", "ntigskov_websrv2-age", "v-JV$H^Aa+1b", "ntigskov_webserv2_age");
-
+$conn = mysqli_connect('localhost', 'ntigskov_websrv2-age', 'v-JV$H^Aa+1b', 'ntigskov_webserv2_age');
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
